c45387f3afbeeac00924ea5c8e07a0b6501fa8b0
ryuimoto <narrow617@gmail.com>
2024-09-02 18:32:04 +0900

    HomePage作成

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage(){
+    return(
+        <div>
+            <h1>Welcome to Maca Shop!</h1>
+            <p>This is your one-stop-shop for all your shopping needs.</p>
+        </div>
+    );
+}
+
+export default HomePage;
